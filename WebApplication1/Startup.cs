49a016239efa1aaffda542bdb5349f6cdc1580ec
Alex Yılmaz <araxis93@gmail.com>
2021-02-15 10:29:39 +0300

    Test

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace WebAPI
+namespace WebApplication1
 {
     public class Startup
     {
